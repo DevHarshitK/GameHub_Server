734701999ecfa98f577fa02a5732281bf69e4935
Harshit Kumar <harshitkumarhk1290@gmail.com>
2025-09-20 11:47:45 +0530

    Game Server

--- src/main/java/com/gaming/Repository/TransactionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.gaming.Repository;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-import com.gaming.Model.Transaction;
-
-@Repository
-public interface TransactionRepository extends MongoRepository<Transaction, String> {
-    // Additional query methods if needed
-}
